7cc5ebfaac9c6a9db5261df28e3dc8974ffe7b65
MelvinMoukam <moukammelvin@gmail.com>
2023-06-12 18:10:19 +0200

    model de l'application

---
+++ src/main/java/com/appemployes/webapp/model/Employe.java
@@ -0,0 +1,18 @@
+package com.appemployes.webapp.model;
+
+import lombok.Data;
+
+@Data
+public class Employe {
+
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String Email;
+
+    private String password;
+}
